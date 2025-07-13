1ee3b543e3a26906f96acda987eb7e66eee2407f
RajPatel676 <rdp676@gmail.com>
2025-07-13 18:14:33 +0530

    changes in some files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 // Get the publishable key from environment variables
-const PUBLISHABLE_KEY = import.meta.env.PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.PUBLISHABLE_KEY; // also use VITE_
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
 }
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
