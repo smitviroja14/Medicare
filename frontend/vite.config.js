cddfda51ac1a8e5e096cdbd582b0358614a486c1
RajPatel676 <rdp676@gmail.com>
2025-07-13 17:40:33 +0530

    solve deployment error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: "/", // 🧠 critical to avoid blank screen
 });
